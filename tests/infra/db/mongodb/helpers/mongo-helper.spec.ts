126a26c7907f56f5b70ebf547c90b3af089867b9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-27 10:07:54 -0300

    test: ensure MongoHelper connects to the database

--- tests/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ tests/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,7 +1,19 @@
+import { MongoClient } from 'mongodb'
+
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
+jest.mock('mongodb', () => ({
+  MongoClient: {
+    connect: jest.fn(),
+    prototype: {
+      db: jest.fn().mockReturnThis(),
+      collection: jest.fn(),
+      close: jest.fn()
+    }
+  }
+}))
+
 describe('MongoHelper', () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let mongoHelper: MongoHelper
 
   beforeEach(() => {
@@ -17,6 +29,13 @@ describe('MongoHelper', () => {
       const instance1 = MongoHelper.getInstance()
       const instance2 = MongoHelper.getInstance()
       expect(instance1).toBe(instance2)
+    })
+  })
+
+  describe('connect', () => {
+    it('Should connect to the database', async() => {
+      await mongoHelper.connect('mongodb://localhost:27017')
+      expect(MongoClient.connect).toHaveBeenCalledWith('mongodb://localhost:27017')
     })
   })
 })
