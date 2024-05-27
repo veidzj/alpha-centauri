0ba11c4bb9aea871c87afc1269a4e91883d38d48
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-27 11:15:35 -0300

    test: ensure MongoHelper does not fails if disconnect is called when client is null

--- tests/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ tests/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -11,26 +11,26 @@ describe('MongoHelper', () => {
     await mongoHelper.disconnect()
   })
 
-  describe('getInstance', () => {
-    it('Should return a singleton instance', () => {
-      const instance1 = MongoHelper.getInstance()
-      const instance2 = MongoHelper.getInstance()
-      expect(instance1).toBe(instance2)
-    })
+  it('Should return a singleton instance', () => {
+    const instance1 = MongoHelper.getInstance()
+    const instance2 = MongoHelper.getInstance()
+    expect(instance1).toBe(instance2)
   })
 
-  describe('connect', () => {
-    it('Should connect to the database', async() => {
-      await mongoHelper.connect(process.env.MONGO_URL ?? '')
-      expect(mongoHelper.getClient()).toBeTruthy()
-    })
+  it('Should connect to the database', async() => {
+    await mongoHelper.connect(process.env.MONGO_URL ?? '')
+    expect(mongoHelper.getClient()).toBeTruthy()
   })
 
-  describe('disconnect', () => {
-    it('Should disconnect from the database', async() => {
-      await mongoHelper.connect(process.env.MONGO_URL ?? '')
-      await mongoHelper.disconnect()
-      expect(mongoHelper.getClient()).toBeNull()
-    })
+  it('Should disconnect from the database', async() => {
+    await mongoHelper.connect(process.env.MONGO_URL ?? '')
+    await mongoHelper.disconnect()
+    expect(mongoHelper.getClient()).toBeNull()
+  })
+
+  it('Should not fail if disconnect is called when client is null', async() => {
+    const mongoHelper = MongoHelper.getInstance()
+    await mongoHelper.disconnect()
+    expect(mongoHelper.getClient()).toBeNull()
   })
 })
