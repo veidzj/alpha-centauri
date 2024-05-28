6e8f4cf5d63c027257046366d52468d44e1fd5e7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-28 17:42:10 -0300

    test: ensure AddExampleMongoRepository throws if mongo throws

--- tests/infra/db/mongodb/example/add-example-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/example/add-example-mongo-repository.spec.ts
@@ -1,4 +1,4 @@
-import { type Collection, ObjectId } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 
 import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb/mongo-test-setup'
 import { AddExampleMongoRepository } from '@/infra/db/mongodb/example'
@@ -32,5 +32,12 @@ describe('AddExampleMongoRepository', () => {
     const account = await exampleCollection.findOne({ _id: new ObjectId(insertedId) })
     expect(count).toBe(1)
     expect(account).toEqual(addExampleRepositoryInput)
+  })
+
+  test('Should throw if mongo throws', async() => {
+    const sut = makeSut()
+    jest.spyOn(Collection.prototype, 'insertOne').mockRejectedValueOnce(new Error())
+    const promise = sut.add(mockAddExampleRepositoryInput())
+    await expect(promise).rejects.toThrow()
   })
 })
