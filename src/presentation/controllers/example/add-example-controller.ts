6aac7ac1974a411d31ae83e4ca9a471b784d3e7c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-28 20:41:17 -0300

    feat: ensure AddExampleController returns status 500 if AddExample throws

--- src/presentation/controllers/example/add-example-controller.ts
+++ src/presentation/controllers/example/add-example-controller.ts
@@ -1,13 +1,17 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { created } from '@/presentation/helpers'
+import { created, serverError } from '@/presentation/helpers'
 import { type AddExample } from '@/domain/usecases/example'
 
 export class AddExampleController implements Controller {
   constructor(private readonly addExample: AddExample) {}
 
   public async handle(request: AddExampleController.Request): Promise<HttpResponse> {
-    const exampleId = await this.addExample.add(request)
-    return created({ exampleId })
+    try {
+      const exampleId = await this.addExample.add(request)
+      return created({ exampleId })
+    } catch (error) {
+      return serverError()
+    }
   }
 }
 
