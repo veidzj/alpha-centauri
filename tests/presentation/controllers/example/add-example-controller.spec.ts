eb0f07e9574b72ea04004610de918b6baf1eb68c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-28 20:43:44 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/example/add-example-controller.spec.ts
+++ tests/presentation/controllers/example/add-example-controller.spec.ts
@@ -4,29 +4,40 @@ import { AddExampleSpy } from '@/tests/domain/mocks/example'
 import { AddExampleController } from '@/presentation/controllers/example'
 import { created, serverError } from '@/presentation/helpers'
 
+interface Sut {
+  sut: AddExampleController
+  addExampleSpy: AddExampleSpy
+}
+
+const makeSut = (): Sut => {
+  const addExampleSpy = new AddExampleSpy()
+  const sut = new AddExampleController(addExampleSpy)
+  return {
+    sut,
+    addExampleSpy
+  }
+}
+
 const mockRequest = (): AddExampleController.Request => ({
   name: faker.person.fullName()
 })
 
 describe('AddExampleController', () => {
   test('Should call AddExample with correct values', async() => {
-    const addExampleSpy = new AddExampleSpy()
-    const sut = new AddExampleController(addExampleSpy)
+    const { sut, addExampleSpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(addExampleSpy.input).toEqual(request)
   })
 
   test('Should return status 201 with example id', async() => {
-    const addExampleSpy = new AddExampleSpy()
-    const sut = new AddExampleController(addExampleSpy)
+    const { sut, addExampleSpy } = makeSut()
     const response = await sut.handle(mockRequest())
     expect(response).toEqual(created({ exampleId: addExampleSpy.output }))
   })
 
   test('Should return status 500 if AddExample throws', async() => {
-    const addExampleSpy = new AddExampleSpy()
-    const sut = new AddExampleController(addExampleSpy)
+    const { sut, addExampleSpy } = makeSut()
     jest.spyOn(addExampleSpy, 'add').mockRejectedValueOnce(new Error())
     const response = await sut.handle(mockRequest())
     expect(response).toEqual(serverError())
