bb98530bd270255904df33e4c76f925b81f0a720
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-29 10:44:56 -0300

    feat: ensure AddExampleController calls Validation with correct values

--- src/presentation/controllers/example/add-example-controller.ts
+++ src/presentation/controllers/example/add-example-controller.ts
@@ -1,12 +1,16 @@
-import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { type Controller, type Validation, type HttpResponse } from '@/presentation/protocols'
 import { created, serverError } from '@/presentation/helpers'
 import { type AddExample } from '@/domain/usecases/example'
 
 export class AddExampleController implements Controller {
-  constructor(private readonly addExample: AddExample) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addExample: AddExample
+  ) {}
 
   public async handle(request: AddExampleController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       const exampleId = await this.addExample.add(request)
       return created({ exampleId })
     } catch (error) {
