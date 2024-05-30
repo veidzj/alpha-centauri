cfe7080577d67d2b08ac6b6090445c7fdb6ea7d3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-30 18:46:32 -0300

    test: ensure RequiredFieldValidation throws ValidationError if field is not provided

---
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker'
+
+import { RequiredFieldValidation } from '@/validation/validators'
+import { ValidationError } from '@/validation/errors'
+
+const makesut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation(fieldName)
+}
+
+const fieldName: string = faker.word.words()
+
+describe('RequiredFieldValidation', () => {
+  test('Should throw ValidationError if field is not provided', () => {
+    const sut = makesut()
+
+    expect(() => {
+      sut.validate({
+        [fieldName]: ''
+      })
+    }).toThrow(new ValidationError(`${fieldName} is required`))
+  })
+})
