8704d9dfe2b16d4cfd21364ba3f32da9f1beb856
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-06-08 12:40:14 -0300

    feat: ensure DbAddAccount throws AccountAlreadyExistsError if CheckAccountByEmailRepository returns true

--- src/application/usecases/account/db-add-account.ts
+++ src/application/usecases/account/db-add-account.ts
@@ -1,11 +1,15 @@
 import { type CheckAccountByEmailRepository } from '@/application/protocols/account'
 import { type AddAccount } from '@/domain/usecases/account'
+import { AccountAlreadyExistsError } from '@/domain/errors/account'
 
 export class DbAddAccount implements AddAccount {
   constructor(private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
 
   public async add(input: AddAccount.Input): Promise<string> {
-    await this.checkAccountByEmailRepository.check(input.email)
+    const accountAlreadyExists = await this.checkAccountByEmailRepository.check(input.email)
+    if (accountAlreadyExists) {
+      throw new AccountAlreadyExistsError()
+    }
     return ''
   }
 }
