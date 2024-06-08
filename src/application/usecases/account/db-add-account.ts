a852552ebdbef291eacd43cd1633eea122520442
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-06-08 14:14:34 -0300

    feat: ensure DbAddAccount calls Hasher with correct password

--- src/application/usecases/account/db-add-account.ts
+++ src/application/usecases/account/db-add-account.ts
@@ -1,15 +1,19 @@
-import { type CheckAccountByEmailRepository } from '@/application/protocols/account'
+import { type CheckAccountByEmailRepository, type Hasher } from '@/application/protocols/account'
 import { type AddAccount } from '@/domain/usecases/account'
 import { AccountAlreadyExistsError } from '@/domain/errors/account'
 
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
+  constructor(
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+    private readonly hasher: Hasher
+  ) {}
 
   public async add(input: AddAccount.Input): Promise<string> {
     const accountAlreadyExists = await this.checkAccountByEmailRepository.check(input.email)
     if (accountAlreadyExists) {
       throw new AccountAlreadyExistsError()
     }
+    await this.hasher.hash(input.password)
     return ''
   }
 }
