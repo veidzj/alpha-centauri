66cdc4906d4a4d03013b6da2b23cfd14beb347d5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-06-08 14:46:53 -0300

    feat: ensure BcryptAdapter calls hash with correct values

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export const env = {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/alpha-centauri',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  salt: process.env.SALT ?? 12
 }
